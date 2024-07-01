26dfcaa8de725cb15357669937fbf308375bf759
Ilya <kravtsov-ilya@mail.ru>
2024-07-01 19:05:13 +0300

    Добавьте файлы проекта.

---
+++ homework6_1/homework6_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#define MODE 1
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	
+#if MODE == 0
+
+	cout << "Работаю в режиме тренеровки." << endl;
+
+#elif MODE == 1
+
+	int x = 0, y = 0;
+
+	cout << "Введите число 1: ";
+	cin >> x;
+	cout << endl;
+	cout << "Введите число 2: ";
+	cin >> y;
+	cout << endl;
+
+	cout << "Результат сложения: " << add(x, y) << endl;
+
+#elif !defined MODE
+#error Константа MODE не определена
+
+#else
+
+	cout << "Неизвестный режим. Завершение работы" << endl;
+
+#endif
+}
